a215b27055d6ede92b6b8714d4b7b574e240f9d6
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-06-24 09:40:36 +0900

    totyyu

---
+++ DrawFunction.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include <KamataEngine.h>
+#include "MassFunction.h"
+#include <Novice.h>
+using namespace KamataEngine;
+struct Sphere{
+	Vector3 center;//中心
+	float radius;
+};
+struct Line{
+	Vector3 origin;//起点
+	Vector3 diff;//方向
+
+};
+struct Ray{
+	Vector3 origin;//起点
+	Vector3 diff;//方向
+};
+struct  Segment{
+	Vector3 origin;//起点
+	Vector3 diff;//方向
+};
+struct Plane
+{
+	Vector3 normal;//法向量
+	float distance;//距離
+};
+
+void DrawSphere(
+	const Sphere& sphere,
+	const Matrix4x4& viewProjectionMatrix,
+	const Matrix4x4& viewportMatrix,
+	uint32_t color
+	
+);
+void DrawGrid(
+	const Matrix4x4& viewProjectionMatrix,
+	const Matrix4x4& viewportMatrix
+);
+void DrawPlane(
+	const Plane& plane,
+	const Matrix4x4& viewProjectionMatrix,
+	const Matrix4x4& viewportMatrix,
+	uint32_t color
+);
+
+Vector3 Project(const Vector3& v1,const Vector3& v2);
+Vector3 ClosestPoint(const Vector3& point,const Segment& segment);
+
+bool IsCollision(const Sphere& si,const Sphere& s2);
+bool IsCollision(const Sphere& sphere,const Plane& plane);
+
--- PBDMesh.h
+++ PBDMesh.h
@@ -1,5 +1,30 @@
-#pragma once
-class PBDMesh
-{
-};
+#pragma once  
+#include "PBD.h"  
+#include <vector> // std::vectorの宣言を追加  
+//#include "MassFunction.h"
+using namespace KamataEngine;
+class PBDMesh  
+{  
+public:  
+	void Initialize(Vector2 startPos, Vector2 endPos, int numPoints, Vector2 gravity);  
+	void Update();  
+	void Draw(const Matrix4x4& viewProjectionMatrix,
+	const Matrix4x4& viewportMatrix);  
 
+	void SetEndPos(Vector2 endPos) {  
+		endPos_ = endPos;  
+	} // Set the end position of the path  
+	void SetStartPos(Vector2 straPos) {  
+		startPos_ = straPos;  
+	} // Set the end position of the path  
+
+private:  
+	Vector2 startPos_; // パスの開始位置  
+	Vector2 endPos_; // パスの終了位置  
+	int numPoints_; // パーティクルの数  
+	const float springStiffness_; // バネの硬さ  
+	const float dt_; // デルタ時間  
+	Vector2 gravity_; // Gravity vector  
+	std::vector<std::vector<PBD::Points>> points_; // パーティクルのリスト  
+	std::vector<std::vector<PBD::Constraint>> constraints_; // 接続のリスト  
+};
